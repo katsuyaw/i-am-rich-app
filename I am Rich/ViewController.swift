f8348388e69ea3f5d1fcd28e016fe20990e13ee3
katsuyaw <kw@kmail.me>
2020-09-06 21:33:51 -0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by kw on 9/6/20.
+//  Copyright © 2020 katsuyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
